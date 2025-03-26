a95301385ccf286356f1abd8842dabf1eae5868c
Raphael Henrique Fernandes Rocha <76854919+raphael-hfRocha@users.noreply.github.com>
2025-03-26 15:10:17 -0300

    Delete src/main/java/org/mercadodominio/Models directory

--- src/main/java/org/mercadodominio/Models/Categoria.java
+++
@@ -1,8 +0,0 @@
-package org.mercadodominio.Models;
-
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-
-public class Categoria extends PanacheEntity {
-    private int CategoriaId;
-    private String CategoriaNome;
-}
